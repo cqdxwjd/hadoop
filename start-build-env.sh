0229be27f44de67009a2c09ba2ff68ec6cb17e95
wangjingdong <cqdxwjd@163.com>
2025-09-05 12:15:01 +0800

    build(docker): 更新 Docker 构建配置以支持 aarch64 架构
    - 修改 Dockerfile_aarch64，使用 hub.hmf.xyz/library/ubuntu:focal 作为基础镜像
    - 更新 apt源为普通 Ubuntu 源，而不是 Ubuntu-Ports
    - 更改 Java 家目录为 amd64 版本
    - 在 docker 构建命令中添加 --platform=linux/amd64 参数

--- start-build-env.sh
+++ start-build-env.sh
@@ -27,7 +27,7 @@ if [[ "$CPU_ARCH" = "aarch64" || "$CPU_ARCH" = "arm64" ]]; then
   DOCKER_FILE="${DOCKER_DIR}/Dockerfile_aarch64"
 fi
 
-docker build -t hadoop-build -f $DOCKER_FILE $DOCKER_DIR
+docker build -t hadoop-build -f $DOCKER_FILE $DOCKER_DIR --platform=linux/amd64
 
 USER_NAME=${SUDO_USER:=$USER}
 USER_ID=$(id -u "${USER_NAME}")
@@ -69,7 +69,7 @@ fi
 # Set the home directory in the Docker container.
 DOCKER_HOME_DIR=${DOCKER_HOME_DIR:-/home/${USER_NAME}}
 
-docker build -t "hadoop-build-${USER_ID}" - <<UserSpecificDocker
+docker build --platform=linux/amd64 -t "hadoop-build-${USER_ID}" - <<UserSpecificDocker
 FROM hadoop-build
 RUN rm -f /var/log/faillog /var/log/lastlog
 RUN groupadd --non-unique -g ${GROUP_ID} ${USER_NAME}
